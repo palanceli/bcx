b6b5e7bc7c8e4d18d9b2621d1befff6f986849a4
palanceli <palanceli@163.com>
2015-08-03 16:18:31 +0800

    更新

---
+++ Startup/iOSP/Sec0701HynoNerd/Sec0601HynoNerd/HypnosisViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sec0601HynoNerd
+//
+//  Created by palance on 15/8/2.
+//  Copyright (c) 2015年 binglen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisViewController : UIViewController
+
+
+@end
+
---
+++ Startup/iOSP/Sec0701HynoNerd/Sec0601HynoNerd/HypnosisViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Sec0601HynoNerd
+//
+//  Created by palance on 15/8/2.
+//  Copyright (c) 2015年 binglen. All rights reserved.
+//
+
+#import "HypnosisViewController.h"
+
+@interface HypnosisViewController ()
+
+@end
+
+@implementation HypnosisViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"HypnosisViewContolller loaded!");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
